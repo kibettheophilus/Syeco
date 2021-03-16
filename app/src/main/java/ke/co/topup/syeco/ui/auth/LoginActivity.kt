a79c0d0e55f64420812496d4a96aba7f45f4a803
Kibet Theophilus <kibettheophilus@gmail.com>
2021-03-16 22:57:23 +0300

    Login Activity functionality

--- app/src/main/java/ke/co/topup/syeco/ui/auth/LoginActivity.kt
+++ app/src/main/java/ke/co/topup/syeco/ui/auth/LoginActivity.kt
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import ke.co.topup.syeco.R
 import ke.co.topup.syeco.databinding.ActivityLoginBinding
 import ke.co.topup.syeco.ui.home.HomeActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -24,8 +26,10 @@ class LoginActivity : AppCompatActivity() {
             finish()
 //            if (binding.emailLogin.text?.trim().toString().isNotEmpty() ||
 //                binding.passwordLogin.text?.trim().toString().isNotEmpty()
-//            ) {
-//
+//            {
+
+//            singInUser(binding.emailLogin.text?.trim().toString(), binding.passwordLogin.text?.trim().toString())
+
 //            }else{
 //                Toast.makeText(this, "Input required", Toast.LENGTH_SHORT).show()
 //            }
@@ -36,5 +40,17 @@ class LoginActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+//        fun signInUser(email: String, password :String){
+//            auth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(this){ task ->
+//                    if (task.isSuccessful){
+//                        val intent = Intent(this, HomeActivity::class.java)
+//                        startActivity(intent)
+//                        finish()
+//                    }else{
+//                        Toast.makeText(this, "Error !!" +task.exception, Toast.LENGTH_SHORT).show()
+//                    }
+//                }
+//        }
     }
 }
